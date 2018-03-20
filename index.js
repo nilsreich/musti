bd05761f369236ee19e68566a74963a3b71afd35
Nils Reich <nilsreich@gmail.com>
2018-03-20 21:54:26 +0100

    jajaja

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+const app = require('express')()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+server.listen(4000, () => {
+  console.log('The server is running: http://localhost:4000')
+})
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+io.on('connection', socket => {
+  console.log('a user connected')
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
+io.on('connection', socket => {
+  socket.on('chat message', msg => {
+    console.log('message: ' + msg)
+  })
+})
+io.emit('some event', {
+  for: 'everyone'
+})
+io.on('connection', socket => {
+  socket.broadcast.emit('hi')
+})
+io.on('connection', socket => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg)
+  })
+})
